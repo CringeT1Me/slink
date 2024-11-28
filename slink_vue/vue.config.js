d41158c523a8f05a912af89f88757c016b016f45
Василий Некрасов <bronyboy2014@gmail.com>
2024-11-28 15:11:02 +0300

    Добавлен workflow для Vue.

--- slink_vue/vue.config.js
+++ slink_vue/vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-const fs = require('fs')
 module.exports = defineConfig({
   transpileDependencies: true
 })
@@ -8,10 +7,10 @@ module.exports = {
   devServer: {
     host: 'localhost',
     port: 8080,
-    https: {
-      key: fs.readFileSync('./certs//localhost+2-key.pem'),
-      cert: fs.readFileSync('./certs//localhost+2.pem'),
-      //ca: fs.readFileSync('./certs/my-ca.crt')
-    },
+    // https: {
+    //   key: fs.readFileSync('./certs//localhost+2-key.pem'),
+    //   cert: fs.readFileSync('./certs//localhost+2.pem'),
+    //   //ca: fs.readFileSync('./certs/my-ca.crt')
+    // },
   }
 };
